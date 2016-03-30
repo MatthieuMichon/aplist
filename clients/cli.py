5d3bf6b19b46b48910c14aca5baf26dde8e43c73
Matthieu Michon <oyaji@ns378363.ip-5-196-68.eu>
2016-03-30 23:29:14 +0200

    Added simple client class

---
+++ clients/cli.py
@@ -0,0 +1,56 @@
+"""Airport List Query package.
+
+Command Line Interface client module.
+"""
+
+
+import argparse
+import aplist
+
+
+def parse():
+    """Parse CLI arguments.
+
+    Returns:
+        (argparse.Namespace): Argument dict.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--search', nargs='+')
+    parser.add_argument('-r', '--sort', nargs='+')
+    parser.add_argument('-o', '--offset', type=int)
+    parser.add_argument('-l', '--limit', type=int)
+    return parser.parse_args()
+
+
+def decode(contents):
+    """Transform a list into a dict.
+
+    Arguments:
+        contents (list): list of pairs.
+
+    Returns:
+        (dict): pairs extracted from the contents list.
+    """
+    retval = {}
+    for item in contents:
+        k, v = item.split('=')
+        if k in retval:
+            raise ValueError('Key "{}" repeated'.format(k))
+        retval[k] = v
+    return retval
+
+
+def main():
+    """Script entry point."""
+    query = {}
+    args = parse()
+    if args.search:
+        query['search'] = decode(args.search)
+
+    apl = aplist.AirportList()
+    retval = apl.query(query=query)
+    print(retval)
+
+
+if __name__ == '__main__':
+    main()
