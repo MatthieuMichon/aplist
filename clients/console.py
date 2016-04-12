ccc097dbe5eed7aaad007acecf189e90cc63ce38
Matthieu Michon <oyaji@ns378363.ip-5-196-68.eu>
2016-04-13 00:27:11 +0200

    basic list of all airports in console client

--- clients/console.py
+++ clients/console.py
@@ -8,22 +8,33 @@ import urwid
 
 
 class Query(object):
+    """Package aplist and helpers in a single class."""
 
     def __init__(self):
+        """Constructor."""
         apl = aplist.AirportList()
         self.retval = apl.query(query={})
 
+    def get_list(self):
+        """Return query result as a list of boxes."""
+        list_ = list(map(
+            urwid.Text, [self.get_result_str(ap) for ap in self.retval]))
+        return list_
+
+    def get_result_str(self, result):
+        """Convert query result into a string."""
+        str_ = ' '.join(map(str, result.values()))
+        return str_
 
 qy = Query()
-al = urwid.ListBox([qy.retval])
+al = urwid.ListBox(qy.get_list())
 
 title = urwid.Text(u'Airport List Query - Console Application',
                    align='center')
 status = urwid.Text(u'F1: Filter - F2: Sort')
 text = (u'bla bla bla')
 
-inner_frame = urwid.Frame(
-    urwid.Filler(al), header=title, footer=status)
+inner_frame = urwid.Frame(al, header=title, footer=status)
 
 outer_frame = urwid.Frame(inner_frame, header=title, footer=status)
 
